cf7560058874a40b91edb0c0de1c9bca9b3bffd2
KaffeLatte <aalto.erik@gmail.com>
2014-04-15 09:42:55 +0200

    added dataExtraction.py and some folders

---
+++ dataExtraction.py
@@ -0,0 +1,53 @@
+import urllib
+import csv
+import pickle
+
+base_url = "http://football-data.co.uk/mmz4281/1314/"
+
+def make_url(ticker_symbol):
+    return base_url + ticker_symbol
+
+output_path = "/afs/nada.kth.se/home/3/u1qvl923/predictPremierLeague"
+def make_filename(ticker_symbol, directory="historicalData"):
+    return output_path + "/" + directory + "/" + ticker_symbol #+ ".csv"
+
+def pull_historical_data(ticker_symbol, directory="historicalData"):
+    try:
+        urllib.urlretrieve(make_url(ticker_symbol), make_filename(ticker_symbol, directory))
+    except urllib.ContentTooShortError as e:
+        outfile = open(make_filename(ticker_symbol, directory), "w")
+        outfile.write(e.content)
+        outfile.close()
+        
+def readDataFromExcel(ticker_symbol):
+    try:
+        with open(output_path+"/historicalData/"+ticker_symbol+'.csv', 'rb') as csvfile:
+            excelReader = csv.reader(csvfile, delimiter=' ', quotechar='|')
+        
+            list = []
+            for row in excelReader:
+                tmpList = []
+                tmpList = ','.join(row)
+                tmpList = tmpList.split(",")
+                list.append(tmpList)
+            
+            print list.pop(0)
+            saveStockDataToFile(ticker_symbol, list)
+            
+    except:
+        print "error when reading/manipulating excel data"
+        
+def saveStockDataToFile(ticker_symbol, list):
+    print "hej"
+    pickle.dump(list, open(output_path+"/objects/"+ticker_symbol, "wb"))
+    print "hej da"
+
+def readStockDataFromFile(ticker_symbol):
+    list = pickle.load(open(output_path+"/objects/"+ticker_symbol, "rb"))
+    
+    return list
+
+pull_historical_data("E0.csv")
+readDataFromExcel("E0")
+list = readStockDataFromFile("E0")
+print list[1]
